0acfd4ece450227f111ec488f57cdddb7d2d2363
Hanna Bacelar <hannabacelar@hotmail.com>
2021-12-25 23:32:35 -0300

    requisito 7, 8 finalizados

--- script.js
+++ script.js
@@ -8,14 +8,23 @@ let capturaButton = document.querySelector("#criar-tarefa")
 capturaButton.addEventListener("click", lidaComInput)
 
 function lidaComInput() {
-    if ( capturandoInput.value !== '') {
-      let criaLi = document.createElement("li")
-      criaLi.innerText = capturandoInput.value
-      capturaOl.appendChild(criaLi)
-      capturandoInput.value = '';
-
-    } 
+    if (capturandoInput.value !== '') {
+        let criaLi = document.createElement("li")
+        criaLi.innerText = capturandoInput.value
+        capturaOl.appendChild(criaLi)
+        capturandoInput.value = '';
+    }
 }
+
+capturaOl.addEventListener('click', aletaraCorDeFundo)
+function aletaraCorDeFundo(event) {
+    let filhosOl = capturaOl.children
+    for (i = 0; i < filhosOl.length; i += 1) {
+        filhosOl[i].style.backgroundColor = 'white';
+    }
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
 
 
 
